f7b13c3df6494626a88b6f3ffcea690aef5b904f
jitendracoder <vickygreat006@gmail.com>
2021-06-16 12:41:37 +0530

    task complete code

--- src/app/itemlist/itemlist.component.ts
+++ src/app/itemlist/itemlist.component.ts
@@ -1,15 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import products from 'products.json';
-// interface itemList { 
-//   index: any,
-//   isSale: boolean,
-//   isExclusive: boolean,
-//   price:any,
-//   productImage:any,
-//   productName:any,
-//   size:[]
 
-// } 
 @Component({
   selector: 'app-itemlist',
   templateUrl: './itemlist.component.html',
@@ -19,21 +10,11 @@ import products from 'products.json';
 export class ItemlistComponent implements OnInit {
 
   constructor() { }
- 
   itemLists = products; 
   filteredString:any;
-  ngOnInit(): void {
-   
-    //this.filterItem(event);
-    console.log(this.filteredString)
-    
-  }
-
+  ngOnInit(): void {}
   filterItem(event){
-    console.log(event.target.value);
     this.filteredString = event.target.value;
-    console.log(this.filteredString)
-
   }
 
 }
